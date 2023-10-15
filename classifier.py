0e08f0a903a62965ec27a5111b31fde4a64e2dc2
Benedict Chan <benedictchan247@gmail.com>
2023-10-15 19:20:58 +0800

    Add counter to find number of occurence for each label

--- classifier.py
+++ classifier.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
 
 df = pd.read_pickle('waferImg26x26.pkl')
 images = df.images.values
 labels = df.labels.values
 labels = np.asarray([str(l[0]) for l in labels])
 
-for label in labels:
-    print(label)
+
+label_counts = Counter(labels)
+
+# Print the label counts
+for label, count in label_counts.items():
+    print(f"Label '{label}': Count = {count}")
+
 # for image in images:
 #     plt.imshow(image.transpose(1, 2, 0))
 #     plt.show()
